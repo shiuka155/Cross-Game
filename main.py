29df789e06eb7c46c08d7fc00b4e404d0255537d
shiuka155 <90806694+shiuka155@users.noreply.github.com>
2023-11-20 22:25:49 -0500

    Create the cars[] list to have bunch of cars, run the loop to let the car move and respawn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 from turtle import Screen
 from player import Player
-from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
@@ -11,14 +10,28 @@ screen.tracer(0)
 screen.bgcolor("black")
 
 # create turtle
-player = Player()
+turtle = Player()
 
 # create response on key
 screen.listen()
-screen.onkey(player.move_up, "Up")
+screen.onkey(turtle.move_up, "Up")
 
+
+# create the scoreboard level
+scoreboard = Scoreboard()
+
+# create car
+cars = [CarManager() for _ in range(10)]
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.05)
     screen.update()
+    # if turtle.ycor() >= 290:
+    #     game_is_on = False
+    #     print("Done")
+    for car in cars:
+        car.move()
+        if car.xcor() < - 300:
+            car.reset_position()
+
